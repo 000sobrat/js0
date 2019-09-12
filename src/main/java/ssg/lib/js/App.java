59eb253aaadae30c39a0611b3b006db4a75fea8f
ssg <000sobrat@gmail.com>
2019-09-12 13:48:16 +0300

    initial

---
+++ src/main/java/ssg/lib/js/App.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ssg.lib.js;
+
+/**
+ *
+ * @author sesidoro
+ */
+public class App {
+
+    public static final void main(String... args) throws Exception {
+        ssg.lib.jana.App.main(args);
+    }
+}
